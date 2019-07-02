222c7034de1d4c4d43a55c171b21be804b95cf19
irikiou <215611352@qq.com>
2019-07-02 11:35:56 +0800

    '项目登录页结构搭建完成'

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入element UI 文件库
 import ElementUI from 'element-ui'
+// 引入CSS样式
 import 'element-ui/lib/theme-chalk/index.css'
+// 为了避免被覆盖，所以把自己写的样式写在最下面
 import '../src/assets/base.css'
 
 Vue.config.productionTip = false
+// 安装elementUI
 Vue.use(ElementUI)
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,11 @@
 <template>
+  <!-- 相当于一个div,用来做布局 -->
+  <!-- type:布局模式，可是是flex -->
+  <!-- span：栅格占据的列数 -->
+  <!-- justify：设置水平对齐的方式 -->
   <el-row type="flex" class="row-bg" justify="center" align="middle">
-    <el-col :span="8">
-      <el-form ref="form" :model="form" label-width="80px" class="loginform" label-position=>
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form ref="form" :model="form" class="loginform" label-position="top" label-width="80px">
         <el-form-item label="用户名">
           <el-input v-model="form.username"></el-input>
         </el-form-item>
@@ -9,8 +13,8 @@
           <el-input v-model="form.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">登录</el-button>
-          <el-button>设置</el-button>
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button>重置</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -26,17 +30,23 @@ export default {
       }
     };
   },
-  methods: {}
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    }
+  }
 };
 </script>
 <style>
 .row-bg {
-  background-color: #2d434c;
   height: 100%;
+  background-color: #2d434c;
 }
 .loginform {
   background-color: #fff;
-  padding: 30px 20px;
   border-radius: 10px;
+  padding: 30px 20px;
+  min-width: 400px;
 }
 </style>
+   
