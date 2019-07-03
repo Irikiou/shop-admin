06ed5f1bf67d90636a11fc07a6aa14118f160ac1
irikiou <215611352@qq.com>
2019-07-03 23:53:26 +0800

    'home侧边栏未完成'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,45 +20,70 @@
     </el-header>
     <el-container>
       <el-aside width="200px">
+        <!--  default-active="2"//设置默认高亮的菜单栏，赋值的内容为菜单项的index值 -->
         <el-menu
           default-active="2"
           class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
         >
+          <!-- el-submenu index="1"：当前菜单项的id,唯一标识 -->
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <span>用户管理</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
+            <!-- el-menu-item:这个组件是每一个没有子菜单项的菜单 -->
+            <el-menu-item index="1-4-1">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </el-menu-item>
           </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <!-- el-menu-item:这个组件是每一个没有子菜单项的菜单 -->
+            <el-menu-item index="1-4-1">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <!-- el-menu-item:这个组件是每一个没有子菜单项的菜单 -->
+            <el-menu-item index="1-4-1">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <!-- el-menu-item:这个组件是每一个没有子菜单项的菜单 -->
+            <el-menu-item index="1-4-1">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <!-- el-menu-item:这个组件是每一个没有子菜单项的菜单 -->
+            <el-menu-item index="1-4-1">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </el-menu-item>
+          </el-submenu>
         </el-menu>
       </el-aside>
       <el-main>Main</el-main>
@@ -69,6 +94,9 @@
 export default {};
 </script>
 <style>
+.el-container {
+  height: 100%;
+}
 .el-container .el-header {
   background-color: #b3c1cd;
   padding: 0;
@@ -91,5 +119,8 @@ export default {};
 }
 .el-container .el-header div a {
   color: #ffa500;
+}
+.el-menu-vertical-demo.el-menu {
+  height: 100%;
 }
 </style>
