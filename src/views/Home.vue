3fc673242fdee7e8a09acfbde71879a95c5ac6df
irikiou <215611352@qq.com>
2019-07-03 23:09:21 +0800

    'home头部完成'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,95 @@
 <template>
-  <div>这是首页</div>
+  <el-container>
+    <el-header>
+      <el-row type="flex" justify="space-between" align="middle">
+        <el-col :span="6">
+          <img src="../assets/logo.png" alt />
+        </el-col>
+        <el-col>
+          <h1>品优购后台管理系统</h1>
+        </el-col>
+        <el-col :span="6">
+          <div class="header-right">
+            <span>
+              欢迎39期星耀会员
+              <a href="#">退出</a>
+            </span>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+<script>
+export default {};
+</script>
+<style>
+.el-container .el-header {
+  background-color: #b3c1cd;
+  padding: 0;
+}
+.el-container .el-header img {
+  width: 200px;
+}
+.el-container .el-header h1 {
+  color: #fff;
+  font-size: 28px;
+  font-weight: bolder;
+  padding: 0;
+  margin: 0;
+  text-align: center;
+}
+.header-right {
+  font-weight: bolder;
+  text-align: right;
+  padding-right: 15px;
+}
+.el-container .el-header div a {
+  color: #ffa500;
+}
+</style>
